918f2ed51bd15bb6b79bbe68df3f134ba2715ab7
gilliantaylor <gilliantaylor@gillians-air.wireless-1x.unc.edu>
2019-08-27 01:21:35 -0400

    yeah woo woo yeah woo (I hope this is the right way).

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -10,5 +10,90 @@ public class A1Adept {
 
 		// Your code follows here.
 		
+		int numberItems = scan.nextInt();
+		
+		// Initializes arrays that will hold the items names and prices
+		
+		String[] items = new String[numberItems];
+		double[] prices = new double[numberItems];
+		
+		// Fills arrays with names and prices
+		
+		for (int i = 0; i < numberItems; i++) {
+			
+			items[i] = scan.next();
+			prices[i] = scan.nextDouble();
+			
+		}
+		
+		int numberCustomers = scan.nextInt();
+		
+		System.out.println("TEST: Number of customers is: " + numberCustomers);
+		
+		double totalSpent = 0;
+		
+		double prevSpent = 0;
+		String topSpender = "placeholder";
+		double topAmount = 0;
+		String lowSpender = "placeholder";
+		double lowAmount = 0;
+		
+		// Gathers information from each customer, saves necessary information
+		
+		for (int j = 0; j < numberCustomers ; j++) {
+			
+			String firstName = scan.next();
+			String lastName = scan.next();
+			
+			System.out.println("TEST: " + firstName);
+			
+			int itemsBought = scan.nextInt();
+			double amountSpent = 0;
+			
+			// Goes through items purchases and stores prices 
+			
+			for (int k = itemsBought; k > 0; k--) {
+				
+				int numberBought = scan.nextInt();
+				String itemBought = scan.next();
+				
+				System.out.println("TEST: Number of " + itemBought + " bought: " + numberBought);
+				
+				
+				for (int l = 0; l < numberItems; l++) {
+					if (itemBought.equals(items[l])) {
+						double cost = numberBought * prices[l];
+						
+						System.out.println("TEST: Total cost: " + cost);
+						
+						amountSpent += cost;
+						
+					}
+				}
+				
+			}
+	
+			
+			System.out.println("TEST: Total amount spent: " + amountSpent);
+			 
+			totalSpent += amountSpent;
+			
+			if (amountSpent >= prevSpent) {
+				topSpender = firstName + " " + lastName;
+				topAmount = amountSpent; 
+				
+			} else {
+				lowSpender = firstName + " " + lastName;
+				lowAmount = amountSpent; 
+			}
+			
+			prevSpent = amountSpent; 
+			
+		}
+		
+		System.out.println("Biggest: " + topSpender + " (" + String.format("%.2f", topAmount) + ")");
+		System.out.println("Smallest: " + lowSpender + " (" + String.format("%.2f", lowAmount) + ")"); 
+		System.out.println("Average: " + String.format("%.2f", (totalSpent/numberCustomers))); 
+		
 	}
 }
